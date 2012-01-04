f9ded9b6e926e4161023f9589b9ca8a7d5294e3c
Brandon Benvie <brandon@bbenvie.com>
2012-01-03 19:20:23 -0500

    Move all keys to bindings, but deleting is broken and I can't figure out why

--- settings/controls.js
+++ settings/controls.js
@@ -1,36 +1,57 @@
 module.exports = {
-  'Command List'      : key('f1'),
+  'Command List'        : key('f1'),
+  'Next Page'           : key('pgdn'),
+  'Previous Page'       : key('pgup'),
+  'Clear Input/Screen'  : key('esc'),
+  'Clear Screen'        : key('esc esc'),
+  'Exit'                : key('esc esc esc'),
 
-  'Create Context'    : ctrl_shift('up'),
-  'Delete Context'    : ctrl_shift('down'),
-  'Next Context'      : ctrl('up'),
-  'Previous Context'  : ctrl('down'),
-  'Reset Context'     : ctrl('r'),
-  'Label Context'     : dot('label'),
+  'Toggle Hiddens'      : key('f2'),
+  'Toggle Builtins'     : key('f3'),
+  'Toggle __proto__'    : key('f4'),
+  'Inspect Depth++'     : key('f5'),
+  'Inspect Depth--'     : key('f6'),
+  'Set Inspect Depth'   : dot('depth'),
+  'Toggle Colors'       : key('f10'),
 
-  'Next Page'         : key('pgdn'),
-  'Previous Page'     : key('pgup'),
+  'Create Context'      : ctrl_shift('up'),
+  'Delete Context'      : ctrl_shift('down'),
+  'Next Context'        : ctrl('up'),
+  'Previous Context'    : ctrl('down'),
+  'Reset Context'       : ctrl('r'),
+  'Label Context'       : dot('label'),
 
-  'Toggle Hiddens'    : key('f2'),
-  'Toggle Builtins'   : key('f3'),
-  'Toggle __proto__'  : key('f4'),
-  'Inspect Depth++'   : key('f5'),
-  'Inspect Depth--'   : key('f6'),
-  'Set Inspect Depth' : dot('depth'),
-  'Toggle Colors'     : key('f10'),
+  'Inject REPL'         : key('f11'),
+  'Toggle Key Display'  : key('f12'),
+  'Require'             : dot('r'),
+  'Generate AST'        : dot('ast'),
 
-  'Inject REPL'       : key('f11'),
-  'Toggle Key Display': key('f12'),
+  'Color Test'          : key('alt+1 alt+2'),
 
-  'Require'           : dot('r'),
+  //'Save Session'      : dot('save'),
 
-  'Clear Input/Screen': key('esc'),
-  'Clear Screen'      : key('esc esc'),
-  'Exit'              : key('esc esc esc'),
-  'Color Test'        : key('alt+1 alt+2'),
+  'Delete Right'        : key('del'),
+  'Delete Left'         : key('bksp'),
+  'Delete Word Right'   : ctrl('del'),
+  'Delete Word Left'    : ctrl('bksp'),
+  'Delete Line Right'   : ctrl_shift('del'),
+  'Delete Line Left'    : ctrl_shift('bksp'),
 
-  'Save Session'      : dot('save'),
+  'Move Left'           : key('left'),
+  'Move Right'          : key('right'),
+  'Word Left'           : ctrl('left'),
+  'Word Right'          : ctrl('right'),
+  'Line Left'           : key('home'),
+  'Line Right'          : key('end'),
+
+  'History Prev'        : key('up'),
+  'History Next'        : key('down'),
+
+  'Line'                : key('enter'),
+  //'Tab Complete'        : key('tab'),
 };
+
+
 
 // ctrl = command on mac
 // alt = meta on linux

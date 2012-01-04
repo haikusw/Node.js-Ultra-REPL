bfd53bb5b59d7aebd1bf97f992b2d8e5b210fb3a
Brandon Benvie <brandon@bbenvie.com>
2012-01-03 22:48:03 -0500

    Not needed

--- bin/ultra-repl.js
+++ bin/ultra-repl.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 var UltraREPL = require('../').UltraREPL;
 
-var fs = require('fs');
-var util = require('util');
-
-try {
-	new UltraREPL
-} catch (e) {
-	fs.writeFileSync('error.log', util.inspect(e, true) + '\n' + util.inspect(global, true, 4));
-}
+new UltraREPL
 

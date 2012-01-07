bb690e9076e31fb5b2ed8ea06b74ef938bc3a8f7
Brandon Benvie <brandon@bbenvie.com>
2012-01-07 18:38:19 -0500

    Fix repldev keybinds

--- modules/repldev.js
+++ modules/repldev.js
@@ -1,9 +1,22 @@
 var style = require('../settings/styling');
 
+function toggle(obj, prop){
+  return function(){
+    if (typeof prop === 'undefined') {
+      var result = (this[obj] ^= true);
+    } else {
+      var result = (this[obj][prop] ^= true);
+    }
+    result = result ? '++' : '--';
+    this.refresh();
+    this.timedPrompt(result + (prop || obj), style.prompt[result]);
+  }
+}
+
 module.exports = [
   { name: 'Inject REPL',
     help: 'Adds a reference to the live repl object to the current context.',
-    defaultTrigger: key('f12'),
+    defaultTrigger: { type: 'keybind', trigger: 'f12' },
     action: function(){
       this.context._ = this.context.ctx.repl = this;
       this.inspector();
@@ -11,12 +24,12 @@ module.exports = [
   },
   { name: 'Toggle Key Display',
     help: 'Toggle displaying what keys are pressed.',
-    defaultTrigger : key('f11'),
+    defaultTrigger: { type: 'keybind', trigger: 'f11' },
     action: toggle('keydisplay')
   },
   { name: 'Color Test',
     help: 'Test ANSI colors\n',
-    defaultTrigger: key('f10'),
+    defaultTrigger: { type: 'keybind', trigger: 'f10' },
     action: function(){
       var width = names.join('').length / 2;
       var left = (this.width - width) / 2;
@@ -39,21 +52,3 @@ module.exports = [
     }
   },
 ]
-
-function toggle(obj, prop){
-  return function(){
-    if (typeof prop === 'undefined') {
-      var result = (this[obj] ^= true);
-    } else {
-      var result = (this[obj][prop] ^= true);
-    }
-    result = result ? '++' : '--';
-    this.refresh();
-    this.timedPrompt(result + (prop || obj), style.prompt[result]);
-  }
-}
-
-
-function keyword(x){ return { type: 'keyword', trigger: x } }
-function dot(x){ return { type: 'command', trigger: '.' + x } }
-function key(x){ return { type: 'keybind', trigger: x } }

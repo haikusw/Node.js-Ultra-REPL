666b4a38bd9cf88efb4c373192a2e0c9de547447
Brandon Benvie <brandon@bbenvie.com>
2012-01-12 17:43:06 -0500

    Very, very basic spidermonkey integration. Allows creation of a spidermonkey context running as a subprocess

---
+++ plugins/spidermonkey.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var path = require('path');
+var spawn = require('child_process').spawn;
+
+var heritable = require('../lib/object-utils').heritable;
+var Context = require('../core/Context');
+var style = require('../settings/styling');
+var isError = require('../lib/object-utils').is('Error');
+
+var loc = path.resolve(__dirname, 'spidermonkey', 'js.exe');
+
+module.exports = [
+  { name: 'Spidermonkey Context',
+    help: 'Create a context that runs code using SpiderMonkey.',
+    defaultTrigger: { type: 'command', trigger: '.sm' },
+    action: function(){
+	    var result = this.context.add(new SpiderMonkeyContext);
+	    if (isError(result)) {
+	      result = result.message.color(style.error);
+	    } else {
+	      result = 'created SpiderMonkeyContext '.color(style.context.create) + result.name;
+	    }
+	    this.rli.timedWrite('topright', result, 'bgbblack');
+	    this.refresh();
+    }
+  }
+];
+
+
+var SpiderMonkeyContext = heritable({
+  constructor: function SpiderMonkeyContext(){
+    var sm = this.spidermonkey = spawn(loc, [], { cwd: process.cwd() });
+  },
+  super: Context,
+  runCode: function runCode(code, filename, finalize){
+  		this.spidermonkey.stdout.once('data', function(data){
+  			finalize({
+  				 status: 'success',
+  				 result: data.toString('utf8'),
+  				 code: code
+  			});
+		});
+  		this.spidermonkey.stdin.write(code + '\n');
+  		return 'async';
+  }
+});

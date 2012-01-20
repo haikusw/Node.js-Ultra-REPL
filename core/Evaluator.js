63e6d2445e4eec72ada77d907abee334d8c9ea7b
Brandon Benvie <brandon@bbenvie.com>
2012-01-20 11:38:44 -0500

    Move vm monkeypatch to a separate function. Clean up evaluation

--- core/Evaluator.js
+++ core/Evaluator.js
@@ -8,40 +8,22 @@ var Context = require('./Context');
 
 var slice = Function.prototype.call.bind(Array.prototype.slice);
 
+
+var tryContext = vm.createContext();
+
+
 module.exports = Evaluator;
 
 function Evaluator(){
   EventEmitter.call(this);
+
   this.contexts = new Iterable;
   this.current = new Context(true);
   this.iterator =  this.contexts.__iterator__();
   this.contexts.add('global', this.current);
   var self = this;
+  patchRunInThisContext(this);
   Object.defineProperty(this.current, 'columns', { get: function(){ return self.columns } });
-
-  vm.runInThisContext = function(context, ritc){
-    return function __runInThisContext(){
-      if (context.ctx === global) {
-        return ritc.apply(this, arguments);
-      } else {
-        var args = slice(arguments);
-        args.splice(1, 1, context.ctx);
-        return vm.runInContext.apply(this, args);
-      }
-    }
-  }(this, vm.runInThisContext);
-
-  vm.Script.prototype.runInThisContext = function(context, ritc){
-    return function __runInThisContext(){
-      if (context.ctx === global) {
-        return ritc.apply(this, arguments);
-      } else {
-        var args = slice(arguments);
-        args.splice(1, 1, context.ctx);
-        return vm.runInContext.apply(this, args);
-      }
-    }
-  }(this, vm.Script.prototype.runInThisContext);
 }
 
 
@@ -51,15 +33,15 @@ Evaluator.prototype = {
 
   get index(){    return this.iterator.current  },
   get count(){    return this.contexts.count()  },
-  get ctx(){      return this.current.ctx       },
   get global(){   return this.current.global    },
+  get ctx(){      return this.current.ctx       },
 
   get builtins(){ return this.current.builtins  },
-  get hiddens(){  return this.current.hiddens  },
+  get hiddens(){  return this.current.hiddens   },
   get protos(){   return this.current.protos    },
   get colors(){   return this.current.colors    },
-  get depth(){    return this.current.depth    },
-  get name(){     return this.current.name     },
+  get depth(){    return this.current.depth     },
+  get name(){     return this.current.name      },
   get _(){        return this.current.ctx._     },
 
   set builtins(v){ this.current.builtins = v },
@@ -120,8 +102,9 @@ Evaluator.prototype = {
       result: this.current.syntaxCheck(code)
     };
 
-    if (output.result.name === 'SyntaxError') {
+    if (output.result instanceof SyntaxError) {
       output.status = 'syntax_error';
+
     } else {
       output.code = output.result;
       try {
@@ -138,3 +121,32 @@ Evaluator.prototype = {
     return output;
   }
 };
+
+
+function patchRunInThisContext(context){
+  vm.runInThisContext = function(ritc){
+    return function __runInThisContext(){
+      if (context.ctx === global) {
+        return ritc.apply(this, arguments);
+      } else {
+        var args = slice(arguments);
+        args.splice(1, 1, context.ctx);
+        return vm.runInContext.apply(this, args);
+      }
+    }
+  }(vm.runInThisContext);
+
+  vm.Script.prototype.runInThisContext = function(ritc){
+    return function __runInThisContext(){
+      if (context.ctx === global) {
+        return ritc.apply(this, arguments);
+      } else {
+        var args = slice(arguments);
+        args.splice(1, 1, context.ctx);
+        return vm.runInContext.apply(this, args);
+      }
+    }
+  }(vm.Script.prototype.runInThisContext);
+}
+
+//(function(){ try { x } catch (e) { return e } })()

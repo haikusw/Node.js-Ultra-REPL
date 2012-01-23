ddcd0484ae13b45d5249d8b507a15983799bab17
Brandon Benvie <brandon@bbenvie.com>
2012-01-23 16:39:40 -0500

    DRY navigation bindings

--- plugins/navigation.js
+++ plugins/navigation.js
@@ -19,81 +19,31 @@ module.exports = [
       }
     }
   },
-  { name: 'Delete Left',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'bksp' },
-    action: function(){ this.rli._deleteLeft() }
-  },
-  { name: 'Delete Right',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'del' },
-    action: function(){ this.rli._deleteRight() }
-  },
-  { name: 'Delete Word Left',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'ctrl+bksp' },
-    action: function(){ this.rli._deleteWordLeft() }
-  },
-  { name: 'Delete Word Right',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'ctrl+del' },
-    action: function(){ this.rli._deleteWordRight() }
-  },
-  { name: 'Delete Line Left',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'ctrl+shift+bksp' },
-    action: function(){ this.rli._deleteLineLeft() }
-  },
-  { name: 'Delete Line Right',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'ctrl+shift+del' },
-    action: function(){ this.rli._deleteLineRight() }
-  },
-  { name: 'Line Left',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'home' },
-    action: function(){ this.rli._lineLeft() }
-  },
-  { name: 'Line Right',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'end' },
-    action: function(){ this.rli._lineRight() }
-  },
-  { name: 'Word Left',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'ctrl+left' },
-    action: function(){ this.rli._wordLeft() }
-  },
-  { name: 'Word Right',
-    defaultTrigger: { type: 'keybind', trigger: 'ctrl+right' },
-    action: function(){ this.rli._wordRight() }
-  },
-  { name: 'Move Left',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'left' },
-    action: function(){ this.rli._moveLeft() }
-  },
-  { name: 'Move Right',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'right' },
-    action: function(){ this.rli._moveRight() }
-  },
+  cmd('Delete Left',        'bksp'),
+  cmd('Delete Right',       'del'),
+  cmd('Delete Word Left',   'ctrl+bksp'),
+  cmd('Delete Word Right',  'ctrl+del'),
+  cmd('Delete Line Left',   'ctrl+shift+bksp'),
+  cmd('Delete Line Right',  'ctrl+shift+del'),
+  cmd('Line Left',          'home'),
+  cmd('Line Right',         'end'),
+  cmd('Word Left',          'ctrl+left'),
+  cmd('Word Right',         'ctrl+right'),
+  cmd('Move Left',          'left'),
+  cmd('Move Right',         'right'),
+  cmd('History Prev',       'up'),
+  cmd('History Next',       'down'),
+  cmd('Line',               'enter'),
+  //cmd('Tab Complete',       'tab'),
+];
 
-  { name: 'History Prev',
-    defaultTrigger: { type: 'keybind', trigger: 'up' },
-    action: function(){ this.rli._historyPrev() }
-  },
-  { name: 'History Next',
+
+function cmd(name, trigger){
+  var fnName = '_'+name[0].toLowerCase() + name.slice(1).replace(/\s/g, '');
+  return {
+    name: name,
     help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'down' },
-    action: function(){ this.rli._historyNext() }
-  },
-  { name: 'Line',
-    help: false,
-    defaultTrigger: { type: 'keybind', trigger: 'enter' },
-    action: function(){ this.rli._line() }
-  },
-  // { name: 'Tab Complete',
-  //   action: function(){ this.rli._tabComplete() }
-  // },
-]
+    defaultTrigger: { type: 'keybind', trigger: trigger },
+    action: function(){ this.rli[fnName]() }
+  }
+}

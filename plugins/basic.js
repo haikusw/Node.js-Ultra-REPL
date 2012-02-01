3ac44383a0ae40f70bd458930f244729b0831a84
Brandon Benvie <brandon@bbenvie.com>
2012-02-01 18:46:11 -0500

    Update to new system for showing command output

--- plugins/basic.js
+++ plugins/basic.js
@@ -17,9 +17,7 @@ module.exports = [
     help: 'Type the name of a built-in module to include it on the current context.',
     defaultTrigger: { type: 'keywords', trigger: builtins.libs },
     action: function(lib){
-      this.context.ctx[lib] = require(lib);
-      this.context.current.refresh();
-      return this;
+      return this.context.ctx[lib] = require(lib);
     }
   },
   { name: 'Require',

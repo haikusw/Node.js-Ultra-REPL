05df09b11c7dc92a26b19496c2be453606ecb600
Brandon Benvie <brandon@bbenvie.com>
2012-02-03 17:25:48 -0500

    Reorganize a bit

--- plugins/debug.js
+++ plugins/debug.js
@@ -1,31 +1,45 @@
 var path = require('path');
 var style = require('../settings/styling');
 
-module.exports = [{
-  name: 'Stack Trace',
-  help: 'View the live stack.',
-  defaultTrigger: { type: 'keybind', trigger: 'alt+s' },
-  action: function(){
-    return getStack().map(trace).join('\n');
-  }
-}];
+module.exports = [
+  {
+    name: 'Stack Trace',
+    help: 'View the live stack.',
+    defaultTrigger: { type: 'keybind', trigger: 'alt+s' },
+    action: function(){
+      this.context.current.refresh();
+      return getStack().map(trace).join('\n');
+    }
+  },
+  // {
+  //   name: 'Stack Trace Fn',
+  //   help: 'View the live stack.',
+  //   defaultTrigger: { type: 'keybind', trigger: '.t' },
+  //   action: function(cmd, fn){
+  //     var result =getStack().find(fn)
+  //     return ~result ? result.map(trace).join('\n') : null;
+  //   }
+  // }
+];
 
-
-var stack_holder = new Error;
-
-function stackPrepare(e, stacks){
+var stack_holder = new Error
+function stackPrepare(e,stacks) {
   var stack = stacks
-  for (var p in stack[0]) {
+  for(var p in stack[0]) {
     stack[p] = stack[0][p]
   }
   stack.find = function(fn) {
-    for (var i=0;i<stack.length;i++) {
-      if (stack[i].getFunction() === fn) return i
+    console.log(stack)
+    for(var i=0;i<stack.length;i++) {
+      if(stack[i].getFunction() === fn) {
+        return i
+      }
     }
-    return -1;
+    return -1
   }
-  return stack;
+  return stack
 }
+
 
 function getStack(){
   var old = Error.prepareStackTrace;
@@ -36,34 +50,36 @@ function getStack(){
   return stack;
 }
 
+
 function trace(_, i){
-  var num = (' #'+(i+1)).color('bblack');
+  var n = (' #'+(i+1)).color('bblack');
   var loc = new Location(_.getLineNumber(),
                          _.getColumnNumber(),
                          _.getPosition(),
                          _.getFileName(),
                          _.getEvalOrigin()).inspect();
 
- var scope =      new Scope(_.isConstructor(),
-                            _.isToplevel(),
-                            _.isNative(),
-                            _.isEval()).inspect()
-                            _.getThis(),
-                            _.getFunction();
+ var scope = new Scope(_.isConstructor(),
+                       _.isToplevel(),
+                       _.isNative(),
+                       _.isEval(),
+                       _.getThis(),
+                       _.getFunction()).inspect();
 
    
- var origin =    new Origin(_.getFunctionName(),
-                            _.getMethodName(),
-                            _.getTypeName()).inspect();
+ var origin = new Origin(_.getFunctionName(),
+                         _.getMethodName(),
+                         _.getTypeName()).inspect();
 
   return [
-    [pad(num, 5), pad(loc[0] + space(1) + loc[2], 60),            pad(loc[3], -10) ],
-    [   space(5), space(60),                                      pad(loc[4], -10) ],
-    [   space(5), pad(origin[0]+space(1)+origin[1], 60),          pad(loc[5], -10) ],
-    [scope]
+    [pad(n,5), pad(loc.origin + space(1) +loc.fileName, 60),  pad(loc.line, -10) ],
+    [space(5), space(60),                            pad(loc.column, -10) ],
+    [space(5), pad(origin.callas+space(1)+origin.callby, 60),pad(loc.position, -10) ],
+    [scope.scopes]
   ].map(function(s){return s.join('')}).join('\n') 
 }
 // pad(scope, -10),
+
 
 
 function space(n){ return repeat(' ', n) }
@@ -113,9 +129,14 @@ Origin.prototype.inspect = function inspect(){
     out[1] = 'as '.color('bred') + out[1]
     out[0] = this.ancestor.color('bblue') + fn;
   }
-  return out;
+  return {
+    callby: out[1],
+    callas: out[0],
+    type: this.type,
+    function: this.fn,
+    ancestor: this.ancestor,
+  }
 }
-process.dpaths = [];
 Origin.prototype.toString = Origin.prototype.inspect;
 
 var natives = process.binding('natives');
@@ -133,7 +154,6 @@ function Location(line, column, pos, file, evalsite){
       this.type = 'js';
     }
   } else {
-    process.dpaths.push(file)
     this.file = path.relative(path.resolve(__filename, '..', 'core'), file);
     this.type = 'user';
   }
@@ -142,51 +162,50 @@ function Location(line, column, pos, file, evalsite){
 }
 
 Location.prototype.inspect = function inspect(){
-  var type, name;
-  var file;
-  var numbers = [this.line,  this.column, this.position];
-  var labels = ['Line', 'Col', 'Pos'];
-  
-  switch (this.type) {
-    case 'node':type = ('['+this.type+']').color('yellow'); file = this.file.color('byellow'); break;
-    case 'js':  type = ('['+this.type+']').color('cyan'); file = this.file.color('bcyan'); break;
-    case 'user':type = ('['+this.type+']').color('green'); file = this.file.color('bgreen'); break;
+  var d = {
+    names: ['Line', 'Col', 'Pos'],
+    trace: [this.line,  this.column, this.position],
+    colors: { node: 'yellow', js: 'cyan', user: 'green' },
+
   }
-
-
-   numbers = numbers.map(function(s){ return (''+s).pad(10).color('white') })
-   labels = labels.map(function(s){ return s.color('bblack').pad(-5) })
+  var typeColor = d.colors[this.type];
+  var type = ('['+this.type+']').color('b'+typeColor);
+  var file = this.file.color(typeColor);
+  d.trace = d.trace.map(function(s){ return (''+s).pad(10).color('white') })
+  d.names = d.names.map(function(s){ return s.color('bblack').pad(-5) })
    
-
-  return [ type,
-           file,
-           this.name.color('bgreen'),
-           labels[0] + space(1) + numbers[0],
-           labels[1] + space(1) + numbers[1],
-           labels[2] + space(1) + numbers[2] ]
-    
-
-
+  return { origin: type,
+           filePath: file,
+           fileName: this.name.color(typeColor),
+           line:     d.names[0] + space(1) + d.trace[0],
+           column:   d.names[1] + space(1) + d.trace[1],
+           position: d.names[2] + space(1) + d.trace[2] };
 };
 
 Location.prototype.toString = Location.prototype.inspect;
 
-function Scope(Constructor, Top, Native, Eval, $this, fn){
-  if (Constructor) this.Constructor = !!Constructor;
-  if (Top) this.Top = !!Top;
-  if (Native) this.Native = !!Native;
-  if (Eval) this.Eval = !!Eval;
-  if ($this) this.$this = $this;
-  if (fn) this.fn = fn;
+function Scope(){
+  for (var k in arguments) {
+    this[Scope.args[k]] = arguments[k];
+  }
 }
 
+Scope.args = ['Constructor', 'Top', 'Native', 'Eval', '$this', 'fn'];
+
 Scope.prototype.inspect = function inspect(){
-  var vals = ['Constructor', 'Top', 'Native', 'Eval'].map(function(s){
-    return s in this ? ('['+s+']').color('magenta') : '';
-  }, this).join('');
-  this.$this && vals.push(this.$this);
-  this.fn && vals.push(this.fn);
-  return vals;
+  var out = {
+    scopes: []
+  }
+  for (var k in arguments) {
+    if (this[Scope.args[k]]) {
+      if (k < 4) {
+        out.scope.push(this[Scope.args[k]].color('magenta'));
+      } else {
+        out[Scope.args[k]] = this[Scope.args[k]];
+      }
+    }
+  }
+  return out;
 }
 
 

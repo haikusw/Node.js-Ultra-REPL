851c8aef7d18c43e6eb35e4b564a70bc9ba2038e
Brandon Benvie <brandon@bbenvie.com>
2012-02-06 17:57:20 -0500

    Ready evaluator to be an event emitter for execution status

--- core/Evaluator.js
+++ core/Evaluator.js
@@ -1,5 +1,6 @@
 var Context = require('./Context');
 var Group = require('../lib/Group');
+var EventEmitter = require('events').EventEmitter;
 
 
 
@@ -20,6 +21,7 @@ function Evaluator(settings){
 }
 
 Evaluator.prototype = {
+  __proto__: EventEmitter.prototype,
   constructor: Evaluator,
 
   get index(){ return this.contexts.index },
@@ -51,6 +53,7 @@ Evaluator.prototype = {
     } else {
       var result = this.contexts.current.run(code, noRecord);
       if (!noRecord) this.lastResult = result;
+      this.emit('execution', result);
       return result;
     }
   },
@@ -58,25 +61,33 @@ Evaluator.prototype = {
   create: function create(isGlobal){
     var context = this.contexts.append(new Context(this.settings, isGlobal));
     this.contexts.last();
+    this.emit('create', context);
     return context;
   },
 
   next: function next(){
-    return this.contexts.next();
+    var context = this.contexts.next();
+    this.emit('change', this.current);
+    return context;
   },
 
   prev: function prev(){
-    return this.contexts.prev();
+    var context = this.contexts.prev();
+    this.emit('change', this.current);
+    return context;
   },
 
   remove: function remove(){
     if (this.current.isGlobal) {
       return new Error("can't remove global");
     }
+    this.emit('remove', this.current);
     return this.contexts.remove();
   },
 
   reset: function reset(){
-    return this.contexts.current.initialize(this.settings);
+    var ctx = this.contexts.current.initialize(this.settings);
+    this.emit('reset', this.current);
+    return ctx;
   },
 };

15a3c970d2253323cd63f381c2ef144654aa1664
Brandon Benvie <brandon@bbenvie.com>
2012-02-08 20:51:18 -0500

    Trying to fix the loader to be more consistent

--- bin/ultra-repl.js
+++ bin/ultra-repl.js
@@ -2,5 +2,8 @@
 var path = require('path');
 var ScopedModule = require('../lib/ScopedModule');
 
-var UltraREPL = ScopedModule._load(path.resolve(__dirname, '../lib/UltraREPL.js'), null, true);
-new UltraREPL;
+setTimeout(function(){
+	var UltraREPL = ScopedModule._load(path.resolve(__dirname, '../lib/UltraREPL.js'), null, true);
+  new UltraREPL;
+}, 50)
+
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var path = require('path');
 var ScopedModule = require('./lib/ScopedModule');
 
-var UltraREPL = ScopedModule._load(__dirname + '/lib/UltraREPL.js', null, true);
-new UltraREPL;
+
+setTimeout(function(){
+  var UltraREPL = ScopedModule._load(__dirname + '/lib/UltraREPL.js', null, true);
+  new UltraREPL;
+}, 50)
+

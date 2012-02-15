bba1a048879a107cc051b348b0a1576cf3e0e560
Brandon Benvie <brandon@bbenvie.com>
2012-02-14 19:40:32 -0500

    Various fixes

--- plugins/esprima.js
+++ plugins/esprima.js
@@ -5,32 +5,17 @@ module.exports = [{
     name: 'Generate AST',
     help: 'Use Esprima to generate AST for code to further debug and modify it.',
     defaultTrigger: api.command('.ast'),
-
     action: function(cmd, target){
-      // search executed code history first
-      var source = this.context.current.history.filter(function(record){
-        if (record.script && record.script.code) {
-          return [ record.script.code, esprima.parse(record.script.code) ];
-        }
-        return ~history.globals.indexOf(target);
-      }, []);
-
-      if (source.length){
-        source = source.map(function(s){ return s.code }).join('\n\n');  // found matching executed code in this context
-      } else if (target in this.context.ctx && typeof this.context.ctx[target] === 'function') {
-        source = this.context.ctx[target] + '';                          // search for matching functions and grab their source
-      } else if (~builtins.libs.indexOf(target)) {
-        source = process.binding('natives')[target];                     // search for builtin Node libs
-      } else if (path.existsSync(target)) {
-        source = fs.readFileSync(target, 'utf-8');                       // search for js files
-      } else {
-        source = target;                                                 // hope that the code itself was passed
+      var code = this.context.run(target).completion;
+      switch (typeof code) {
+        case 'function': code += ''; break;
       }
-      try {
-        return this.context.ctx.ast = esprima.parse(source).body;
-      } catch (e) {
-        return e;
+      if (code) {
+        var ast = esprima.parse(code);
+        code = esprima.generate(ast, { indent: '  ' });
+        return highlight(code);
       }
+      return '';
     },
   },
   // { name: 'Analyze Input',
